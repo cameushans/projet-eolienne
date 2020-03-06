e0be8774b1a497721630b105380600871ecf47fd
Biboys999 <mohamedrayane.aboussaid@gmail.com>
2020-03-06 12:30:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Objet draggable
+import Pacman from './img/pacman-151558_1280.png'
+import Bouclier from './img/bouclier.png'
+import Crane from './img/crane.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <h1>Title</h1>
+      <section className="game-app">
+        <section className="col-item wrap">
+
+          <div className="rotor"></div>
+          <div className="mat INDIANRED"></div>
+          <div className="triangle"></div>
+          <div className="socle maroon"></div>
+
+          <div className="rotor"></div>
+          <div className="triangle"></div>
+          <div className="socle maroon"></div>
+          <div className="mat INDIANRED"></div>
+
+
+        </section>
+
+
+        <section className="col-item wrap">
+
+          <div className="rotor"></div>
+          <div className="mat INDIANRED"></div>
+          <div className="socle maroon"></div>
+          <div className="triangle"></div>
+
+          <div className="rotor"></div>
+          <div className="triangle"></div>
+          <div className="mat INDIANRED"></div>
+          <div className="socle maroon"></div>
+        </section>
+      </section>
+
+
+    </body>
   );
 }
 
 export default App;
+
+
+{/* <div className="paysage h150px w150px" style={{backgroundImage: `url(${Pacman})`}}></div>
+        <div className="paysage h150px w150px" style={{backgroundImage: `url(${Bouclier})`}}></div>
+        <div className="paysage h150px w150px" style={{backgroundImage: `url(${Crane})`}}></div> */}
