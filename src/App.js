d39ea420c918d2bfdee73ca4b65ad724f9feba18
Biboys999 <mohamedrayane.aboussaid@gmail.com>
2020-03-09 16:36:03 +0100

    drag & drop fonctionel sur les colonnes

--- src/App.js
+++ src/App.js
@@ -1,70 +1,104 @@
 import React from 'react';
 import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
 
 
 
 
-class App extends React.Component{
-  render(){
-    function allowDrop(ev) {
-      ev.preventDefault();
-      }
-    
-      function drag(ev) {
-      ev.dataTransfer.setData("text", ev.target.id);
-      }
-    
-      function drop(ev) {
-      ev.preventDefault();
-      var data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(data));
-    }
-    return (
-      <body>
-        <h1>Title</h1>
+function App(){
+  return (
+    <div className="App">
+      <main className="flexbox">
+        <Board id="board-1" className="board">
 
-        <section className="game-app">
+          <Card id="card-1" className="triangle card"  style={{color: '#CD5C5C'}} draggable="true" />
+          <Card id='card-2' className="triangle card"  style={{color: '#CD5C5C'}} draggable='true' />
+          <Card id="card-10" className="socle card"  style={{backgroundColor: '#CD5C5C'}} draggable="true" />
 
-          <section className="col-item wrap">
-            <div className="rotor"></div>
-            <div id="drag1" className="mat SALMON   draggable"      draggable="true" onDragStart="drag(event)"></div>
-            <div id="drag2" className="triangle     draggable"      draggable="true" onDragStart="drag(event)"></div>
-            <div id="drag3" className="socle maroon draggable"      draggable="true" onDragStart="drag(event)"></div>
 
-            <div className="rotor"></div>
-            <div id="drag5" className="triangle       draggable"     draggable="true" onDragStart="drag(event)"></div>
-            <div id="drag6" className="socle pink     draggable"     draggable="true" onDragStart="drag(event)"></div>
-            <div id="drag7" className="mat INDIANRED  draggable"     draggable="true" onDragStart="drag(event)"></div>
+          {/* <Card id="card-2" className="card" draggable="true">
+            <p>Hello 2</p>
+          </Card> */}
 
-            {/* // Example
-            <div id="drag2" class="draggable" draggable="true" ondragstart="drag(event)"></div>
-                      
-            <div class="dropper margin" ondrop="drop(event)" ondragover="allowDrop(event)"></div> */}
-          </section>
+        </Board>
 
-          <section className="dropper zone" onDrop="drop(event)" onDragOver="allowDrop(event)">
+        <Board id="board-2" className="board">
 
-          </section>
+          <Card id="card-7" className="mat card"       style={{backgroundColor: '#CD5C5C'}} draggable="true" />
+          <Card id='card-8' className="triangle card"  style={{color: '#CD5C5C'}} draggable='true' />
 
-          <section className="col-item wrap">
-            <div className="rotor"></div>
-            <div className="mat SALMON draggable"     draggable="true"></div>
-            <div className="socle maroon draggable"   draggable="true"></div>
-            <div className="triangle draggable"       draggable="true"></div>
 
-            <div className="rotor"></div>
-            <div className="triangle draggable"       draggable="true"></div>
-            <div className="mat INDIANRED draggable"  draggable="true"></div>
-            <div className="socle pink draggable"     draggable="true"></div>
-          </section>
-        </section>
-
-      </body>
-    );
-  }
+        </Board>
+      </main>
+    </div>
+  );
 }
-
 export default App;
+
+
+
+// class App extends React.Component{
+//   render(){
+//     function allowDrop(ev) {
+//       ev.preventDefault();
+//       }
+    
+//       function drag(ev) {
+//       ev.dataTransfer.setData("text", ev.target.id);
+//       }
+    
+//       function drop(ev) {
+//       ev.preventDefault();
+//       var data = ev.dataTransfer.getData("text");
+//       ev.target.appendChild(document.getElementById(data));
+//     }
+//     return (
+//       <body>
+//         <h1>Title</h1>
+
+//         <section className="game-app">
+
+//           <section className="col-item wrap">
+//             <div className="rotor"></div>
+//             <div id="drag1" className="mat SALMON   draggable"      draggable="true" onDragStart="drag(event)"></div>
+//             <div id="drag2" className="triangle     draggable"      draggable="true" onDragStart="drag(event)"></div>
+//             <div id="drag3" className="socle maroon draggable"      draggable="true" onDragStart="drag(event)"></div>
+
+//             <div className="rotor"></div>
+//             <div id="drag5" className="triangle       draggable"     draggable="true" onDragStart="drag(event)"></div>
+//             <div id="drag6" className="socle pink     draggable"     draggable="true" onDragStart="drag(event)"></div>
+//             <div id="drag7" className="mat INDIANRED  draggable"     draggable="true" onDragStart="drag(event)"></div>
+
+//             {/* // Example
+//             <div id="drag2" class="draggable" draggable="true" ondragstart="drag(event)"></div>
+                      
+//             <div class="dropper margin" ondrop="drop(event)" ondragover="allowDrop(event)"></div> */}
+//           </section>
+
+//           <section className="dropper zone" onDrop="drop(event)" onDragOver="allowDrop(event)">
+
+//           </section>
+
+//           <section className="col-item wrap">
+//             <div className="rotor"></div>
+//             <div className="mat SALMON draggable"     draggable="true"></div>
+//             <div className="socle maroon draggable"   draggable="true"></div>
+//             <div className="triangle draggable"       draggable="true"></div>
+
+//             <div className="rotor"></div>
+//             <div className="triangle draggable"       draggable="true"></div>
+//             <div className="mat INDIANRED draggable"  draggable="true"></div>
+//             <div className="socle pink draggable"     draggable="true"></div>
+//           </section>
+//         </section>
+
+//       </body>
+//     );
+//   }
+// }
+
+// export default App;
 
 {/* <div className="paysage h150px w150px" style={{backgroundImage: `url(${Pacman})`}}></div>
         <div className="paysage h150px w150px" style={{backgroundImage: `url(${Bouclier})`}}></div>
---
+++ src/components/Card.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Card(props) {
+    const dragStart = e => {
+        const target = e.target
+        console.log(e)
+        console.log(e.target.id)
+        e.dataTransfer.setData('card-id', target.id);
+
+
+        // setTimeout(() => {
+        //     target.style.display = "none";
+        // }, 0);
+    }
+
+    const dragOver = e => {
+        e.stopPropagation();
+    } 
+
+    const dragLeave = e => {
+        e.stopPropagation();
+        e.target.style.display = "block";
+    }
+
+     
+    return (
+        <div
+        id={props.id}
+        className={props.className}
+        style={props.style}
+        draggable={props.draggable}
+        onDragStart={dragStart}
+        onDragOver={dragOver}
+        onDragLeave={dragLeave}
+        >
+            { props.children }
+        </div>
+
+        // <div
+        // id={props.id}
+        // className={props.className}
+        // draggable={props.draggable}
+        // onDragStart={dragStart}
+        // onDragOver={dragOver}
+        // onDragLeave={dragLeave}
+        // >
+        //     { props.children }
+        // </div>
+    )
+}
+
+export default Card
