8b225c859d6424db69311e8f4984ad7d0ae01ee4
Biboys999 <mohamedrayane.aboussaid@gmail.com>
2020-05-26 21:41:31 +0200

    intégration design compo shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,26 +5,50 @@ import StripeAchat  from "./StripeAchat";
 class Shop extends Component {
     render() {
         return (
-            <body>
-                <header>
-
-                </header>
-                <main>
+            <body className="">
+                <nav id="nav">
+        {/*            <ul className="row">
+                        <li className="white_color li_nav">Name</li>
+                        <li className="white_color li_nav">Name</li>
+                        <li className="white_color li_nav">Name</li>
+                    </ul>
+        */}
+                </nav>
+                <main id="main_shop">
                     <h2>Shop Luxéole</h2>
-                    <section>
+                    <section className="row around">
                         <div className="card2">
 
                         </div>
-                        <div className="bloc_item"></div>
-                        <div className="bloc_item"></div>
+                        <div className="card2"></div>
+                        <div className="card2"></div>
+                    </section>
+
+                    <section class="container content-section">
+                        <div class="cart-row">
+                            <span class="cart-item cart-header cart-column">ITEM</span>
+                            <span class="cart-price cart-header cart-column">PRICE</span>
+                            <span class="cart-quantity cart-header cart-column">QUANTITY</span>
+                        </div>
+                        <div class="cart-items">
+                        </div>
+                        <div class="cart-total">
+                            <strong class="cart-total-title">Total</strong>
+                            <span class="cart-total-price">$0</span>
+                        </div>
+                        <button class="btn btn-primary btn-purchase" type="button">PURCHASE</button>
                     </section>
                 </main>
-                <footer>
-                    <StripeAchat />
+                <footer id="footer_shop" className="row">
+                    <div>
+                    <input />
+                    <button>Envoie</button>
+                    </div>
                 </footer>
-            </body>
-        )
-    }
-}
-
-export default Shop
+                </body>
+                )
+            }
+        }
+        
+        export default Shop
+        // <StripeAchat />
