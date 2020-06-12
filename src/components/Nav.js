aba565ebac282893f6c237276d1586addd6f39d4
Biboys999 <mohamedrayane.aboussaid@gmail.com>
2020-06-12 05:47:06 +0200

    icon home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,14 @@ import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 export class Nav extends Component {
     render() {
         return (
             <nav id="nav">
             <ul className="row flex_end">
+                <NavLink to="/"><li style={{position: "absolute", left: 20}} className="li_nav"> <FontAwesomeIcon size="3x" color="white" icon={faHome} /> </li> </NavLink>
                 <a href="https://Paypal.me/CarnetinleParc"><li className="li_nav"> <FontAwesomeIcon size="3x" color="white" icon={faHandHoldingUsd} /> </li> </a>
                 <li className="li_nav"> Soutenir le Projet <br/> <br/> Soutenir les developpeur</li>
                 <NavLink to="/shop"><li className="li_nav"> <FontAwesomeIcon size="3x" color="white" icon={faCartArrowDown} /> </li> </NavLink>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -13,58 +13,60 @@ class Shop extends Component {
             <body className="">
                 <Nav />
                 <main>
-                    <h2 style={{textAlign: 'center'}}>Shop Luxéole</h2>
+                    <h2 className="margin50" style={{textAlign: 'center'}}>Shop Luxéole</h2>
                     <section className="row around wrap">
                         <div className="card2 info wrap">
                             <img className="photographie" src={rotor3D} />
                             <div className="row area-text">
-                                <span id="description_produit">Description</span>
-                                <span id="price">3€</span>
+                                <button className="btn btn-primary shop-item-button" type="button">ADD TO ARTICLE</button>
+                                <span>10€</span>
                             </div>
                             <div className="wesh">contraction de Wireless Fidelity<br/>2ème ligne </div>
                         </div>
                         <div className="card2 info"> 
                             <img className="photographie" src={socle3D} />
                             <div className="row area-text">
-                                <span id="description_produit">Description</span>
-                                <span id="price">3€</span>
+                                <button className="btn btn-primary shop-item-button" type="button">ADD TO ARTICLE</button>
+                                <span>10€</span>
                             </div>
                             <div className="wesh">contraction de Wireless Fidelity<br/>2ème ligne </div>
                         </div>
                         <div className="card2 info"> 
                             <img className="photographie" src={rotor3D} />
                             <div className="row area-text">
-                                <span id="description_produit">Description</span>
-                                <span id="price">3€</span>
+                                <button className="btn btn-primary shop-item-button" type="button">ADD TO ARTICLE</button>
+                                <span>10€</span>
                             </div>
                             <div className="wesh">contraction de Wireless Fidelity<br/>2ème ligne </div>
                         </div>
                     </section>
 
-                    <section class="container content-section">
-                        <div class="cart-row">
-                            <span class="cart-item cart-header cart-column">ITEM</span>
-                            <span class="cart-price cart-header cart-column">PRICE</span>
+                    <section className="container content-section margin50">
+                        <div className="cart-row">
+                            <span className="cart-item cart-header cart-column">ITEM</span>
+                            <span className="cart-price cart-header cart-column">PRICE</span>
                         </div>
-                        <div class="cart-items">
+                        <div className="cart-items">
                         </div>
-                        <div class="cart-total">
-                            <strong class="cart-total-title">Total</strong>
-                            <span class="cart-total-price">0€</span>
+                        <div className="cart-total">
+                            <strong className="cart-total-title">Total</strong>
+                            <span className="cart-total-price">0€</span>
                         </div>
                         <StripeAchat />
                     </section>
                 </main>
-                <footer id="footer_shop" className="row">
-                    <form className="form__group">
-                    <input className="form__field"/>
-                    <label for="name" class="form__label">Name</label>
-                    <button className="form__btn">Envoie</button>
-                    </form>
-                </footer>
                 </body>
                 )
             }
         }
         
         export default Shop
+
+
+    //     <footer id="footer_shop" className="row">
+    //     <form className="form__group">
+    //     <input className="form__field"/>
+    //     <label for="name" class="form__label">Name</label>
+    //     <button className="form__btn">Envoie</button>
+    //     </form>
+    //     </footer>
